470e1b5325b58e9763779d0b1c54d17e8250ca4e
KiruthikaChandran <kiruthikachandran11@gmail.com>
2024-10-02 18:16:06 +0530

    complete

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-           <footer className='py-3 bg-dark'>
+           <footer className='py-1 bg-dark'>
             <div className='container'>
                 <p className='m-0 text-center text-white'>
                     Innovation for a Better Future 🤍
