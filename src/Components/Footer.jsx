de27c4477e64841fd027ead63b9e3ece16832755
KiruthikaChandran <kiruthikachandran11@gmail.com>
2024-10-02 18:13:56 +0530

    complete

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,9 +5,9 @@ const Footer = () => {
         <div>
            <footer className='py-3 bg-dark'>
             <div className='container'>
-                <p className='m-0 text-center text-aqua'><i>
+                <p className='m-0 text-center text-white'>
                     Innovation for a Better Future 🤍
-                </i></p>
+                </p>
             </div>
            </footer>
         </div>
